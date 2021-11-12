ad2ab56d0cc4718b6ac9694844ba7a4fef5727f2
Tony <awalker780@gmail.com>
2021-11-11 20:22:17 -0500

    Added spinner animation

---
+++ spinner1.js
@@ -0,0 +1,21 @@
+const process = require("process");
+
+setTimeout(() => {
+  process.stdout.write("\r| ")
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write("\r/ ")
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write("\r- ")
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write("\r\\ ")
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write("\r| ")
+}, 900);
